1be507f81387bffad7a84dc2315098a198f7a9c3
Anwar Muhamat <anwar.runs@gmail.com>
2019-11-07 10:05:12 +0700

    fix db connection

--- run.sh
+++ run.sh
@@ -33,7 +33,7 @@ BACKUP_NAME=\${TIMESTAMP}.dump.gz
 S3BACKUP=${S3PATH}\${BACKUP_NAME}
 S3LATEST=${S3PATH}latest.dump.gz
 echo "=> Backup started"
-if mongodump --host ${MONGODB_HOST} --port ${MONGODB_PORT} ${USER_STR}${PASS_STR}${DB_STR} --archive=\${BACKUP_NAME} --gzip ${EXTRA_OPTS} && aws $AWS_ARGS s3 cp \${BACKUP_NAME} \${S3BACKUP} && aws $AWS_ARGS s3 cp \${S3BACKUP} \${S3LATEST} && rm \${BACKUP_NAME} ;then
+if mongodump --host ${MONGODB_HOST} --port ${MONGODB_PORT} -u ${MONGODB_USER} -p ${MONGODB_PASS} -d ${MONGODB_DB} --authenticationDatabase admin --archive=\${BACKUP_NAME} --gzip ${EXTRA_OPTS} && aws $AWS_ARGS s3 cp \${BACKUP_NAME} \${S3BACKUP} && aws $AWS_ARGS s3 cp \${S3BACKUP} \${S3LATEST} && rm \${BACKUP_NAME} ;then
     echo "   > Backup succeeded"
 else
     echo "   > Backup failed"
@@ -54,7 +54,7 @@ else
 fi
 S3RESTORE=${S3PATH}\${RESTORE_ME}
 echo "=> Restore database from \${RESTORE_ME}"
-if aws s3 cp \${S3RESTORE} \${RESTORE_ME} && mongorestore --host ${MONGODB_HOST} --port ${MONGODB_PORT} ${USER_STR}${PASS_STR}${DB_STR} --drop ${EXTRA_OPTS} --archive=\${RESTORE_ME} --gzip && rm \${RESTORE_ME}; then
+if aws s3 cp \${S3RESTORE} \${RESTORE_ME} && mongorestore --host ${MONGODB_HOST} --port ${MONGODB_PORT} -u ${MONGODB_USER} -p ${MONGODB_PASS} -d ${MONGODB_DB} --authenticationDatabase admin --drop ${EXTRA_OPTS} --archive=\${RESTORE_ME} --gzip && rm \${RESTORE_ME}; then
     echo "   Restore succeeded"
 else
     echo "   Restore failed"
